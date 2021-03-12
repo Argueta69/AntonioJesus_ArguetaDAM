0a3c4f895d33e971eab81e1b4d3f168a2c152af2
Argueta01 <ajarguetab01@iesalbarregas.es>
2021-03-12 18:48:58 +0100

    Línea en método main

--- AntonioJesus_ArguetaDAM/src/Principal.java
+++ AntonioJesus_ArguetaDAM/src/Principal.java
@@ -3,7 +3,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Línea 1");
+		System.out.println("Línea 2");
 	}
 
 }
